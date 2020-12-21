9cde01ac13ba784572c0b389d8078c6436f62c3d
tomblong <tomblong@users.noreply.github.com>
2020-12-21 21:55:23 +0700

    Add project files.

---
+++ ServerConfigRest_019_HanifFauzanAlbana/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceRest_019_HanifFauzanAlbana;
+
+namespace ServerConfigRest_019_HanifFauzanAlbana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostobjek = null;
+
+            try
+            {
+                hostobjek = new ServiceHost(typeof(TI_UMY));
+                hostobjek.Open();
+                Console.WriteLine("Server sampun siap lurr...");
+                Console.ReadLine();
+                hostobjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostobjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
